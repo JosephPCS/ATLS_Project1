faa904c4a012c614db529650ac41c9db7f51b18f
JosephPCS <89505711+JosephPCS@users.noreply.github.com>
2024-10-22 10:46:37 -0600

    thruster code

--- Assets/Scripts/thruster.cs
+++ Assets/Scripts/thruster.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class thruster : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rb;
+
+    public GameObject thrustertemp;
+    public float thrustPow = 10.0f;
+
+    public InputActionReference thrust;
+
+    bool isThrust = false;
     void Start()
     {
-        
+        if(rb == null){
+            rb = GetComponent<Rigidbody>();
+        }
+        thrust.action.performed += ctx => isThrust = true;
+        thrust.action.canceled += ctx => isThrust = false;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isThrust){
+            rb.AddForce(thrustPow*transform.forward, ForceMode.Impulse);
+        }
+            
     }
 }
