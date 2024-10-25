837acdcf6506a09e8c7676e6f683a607341937f0
Nathan Khazam <nakh6169@colorado.edu>
2024-10-25 13:25:01 -0700

    Thrusters working

--- Assets/Scripts/thruster.cs
+++ Assets/Scripts/thruster.cs
@@ -6,30 +6,32 @@ using UnityEngine.InputSystem;
 public class thruster : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     public Rigidbody rb;
-
-    public GameObject thrustertemp;
     public float thrustPow = 10.0f;
-
     public InputActionReference thrust;
-
     bool isThrust = false;
+    private Vector3 noVertical;
+    private bool alreadyThrusted = false;
+
     void Start()
     {
-        if(rb == null){
-            rb = GetComponent<Rigidbody>();
-        }
         thrust.action.performed += ctx => isThrust = true;
         thrust.action.canceled += ctx => isThrust = false;
-       
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(isThrust){
-            rb.AddForce(thrustPow*transform.forward, ForceMode.Impulse);
+        //Debug.Log(isThrust);
+
+        if(isThrust && !alreadyThrusted){
+            noVertical = new Vector3(transform.forward[0], 0, transform.forward[2]);
+            rb.AddForce(thrustPow * noVertical * -1, ForceMode.Impulse);
+            alreadyThrusted = true;
         }
-            
+        if(!isThrust){
+            alreadyThrusted = false;
+        }
     }
 }
